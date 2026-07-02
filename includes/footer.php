a32f67a733efcdfab49ce3dc50721b034c8386c7
Juan Galindo <juangerardogalindopineda@gmail.com>
2026-07-01 22:48:01 -0600

    Proyecto TecnoDesk con dashboard, modo oscuro/claro y mejoras UI

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <p>&copy; 2026 TecnoDesk. Proyecto Unidad 1 - Desarrollo Web Profesional.</p>
 </footer>
 
-<script src="js/oop-tecnodesk.js"></script>
+<script src="js/oop-tecnodesks.js"></script>
 
 </body>
 </html>
